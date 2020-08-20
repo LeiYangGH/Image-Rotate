c025f59ef35be4f696752c3b0374ed8c22ea23ee
yang.lei <yang.lei@nuance.com>
2020-08-20 09:11:52 +0800

    save black

--- ImageRotator/MainWindow.xaml.cs
+++ ImageRotator/MainWindow.xaml.cs
@@ -33,6 +33,18 @@ namespace ImageRotator
             InitializeComponent();
         }
 
+        private void OpenImageFile(string fileName)
+        {
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.StreamSource = new MemoryStream(File.ReadAllBytes(fileName));
+            img.EndInit();
+            this.originalImg = img;
+            this.img.Source = img;
+            this.img.Width = this.originalImg.Width + this.originalImg.Height;
+            this.img.Height = this.originalImg.Width + this.originalImg.Height;
+        }
+
         private void btnOpen_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog dialog = new OpenFileDialog();
@@ -41,15 +53,9 @@ namespace ImageRotator
             if (!dialog.ShowDialog() ?? false)
                 return;
             this.inputImageFileName = dialog.FileName;
+            OpenImageFile(this.inputImageFileName);
             //string fileName = @"C:\Users\LeiYang\Downloads\无标题.bmp";
-            BitmapImage img = new BitmapImage();
-            img.BeginInit();
-            img.StreamSource = new MemoryStream(File.ReadAllBytes(inputImageFileName));
-            img.EndInit();
-            this.originalImg = img;
-            this.img.Source = img;
-            this.img.Width = this.originalImg.Width+ this.originalImg.Height;
-            this.img.Height = this.originalImg.Width + this.originalImg.Height;
+
         }
 
         private void sldAngle_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -89,7 +95,7 @@ namespace ImageRotator
         //    }
         //}
 
-        private void btnSave_Click(object sender, RoutedEventArgs e)
+        private void SaveRotateImage()
         {
             double angle;
             if (double.TryParse(this.txtAngle.Text.Trim(), out angle))
@@ -107,6 +113,11 @@ namespace ImageRotator
             }
         }
 
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveRotateImage();
+        }
+
 
 
         void SaveToBmp(Image visual, string fileName)
@@ -119,35 +130,46 @@ namespace ImageRotator
 
         void SaveUsingEncoder(Image imgControl, string fileName, BitmapEncoder encoder)
         {
-            int hw = (int)Math.Sqrt( Math.Pow( this.originalImg.Width,2)  + Math.Pow(this.originalImg.Height,2));
-            RenderTargetBitmap bitmap1 = new RenderTargetBitmap(hw, hw, 96, 96, PixelFormats.Default);
-            //RenderTargetBitmap bitmap2 = new RenderTargetBitmap(hw, hw, 96, 96, PixelFormats.Default);
-
+            imgControl.UpdateLayout();
+            //int hw = (int)Math.Sqrt(Math.Pow(this.originalImg.Width, 2) + Math.Pow(this.originalImg.Height, 2));
+            //int hw = (int)Math.Sqrt(Math.Pow(this.originalImg.Width, 2) + Math.Pow(this.originalImg.Height, 2));
+            //RenderTargetBitmap bitmap1 = new RenderTargetBitmap(hw, hw, 96, 96, PixelFormats.Default);
+            RenderTargetBitmap bitmap2 = new RenderTargetBitmap((int)imgControl.ActualWidth, (int)imgControl.ActualHeight, 96, 96, PixelFormats.Default);
+            Rect bounds =  VisualTreeHelper.GetDescendantBounds(imgControl);
             //using (Graphics bitmapBuffer = Graphics.FromImage(bitmap))
             //{
             //    System.Drawing.Brush brush = new SolidBrush(System.Drawing.Color.FromArgb(127, 255, 255, 255));
             //    bitmapBuffer.FillRectangle(brush, 10, 10, 80, 80);
             //}
 
-            DrawingVisual drawingVisual = new DrawingVisual();
-            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            //DrawingVisual drawingVisual = new DrawingVisual();
+            //using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            //{
+            //    Brush visualBrush = Brushes.White;
+            //    drawingContext.DrawRectangle(visualBrush, null,
+            //      new Rect(new Point(), new Size(hw, hw)));
+            //    drawingContext.DrawDrawing(new ImageDrawing(imgControl.Source, new Rect(new Point(), new Size(hw, hw))));
+            //    drawingContext.DrawDrawing(imgControl.);
+            //}
+
+            DrawingVisual drawingVisual2 = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual2.RenderOpen())
             {
-                Brush visualBrush = Brushes.White;
+                Brush visualBrush = new VisualBrush(imgControl);
                 drawingContext.DrawRectangle(visualBrush, null,
-                  new Rect(new Point(), new Size(hw, hw)));
-                drawingContext.DrawDrawing(new ImageDrawing(imgControl.Source, new Rect(new Point(), new Size(hw, hw))));
-                drawingContext.DrawDrawing(imgControl.);
+                  new Rect(new Point(), bounds.Size));
+                
             }
 
-
             //bitmap.Render(drawingVisual);
-            bitmap1.Render(drawingVisual);
-            //bitmap2.Render(visual);
-            BitmapFrame frame1 = BitmapFrame.Create(bitmap1);
-            encoder.Frames.Add(frame1);
-            //BitmapFrame frame2 = BitmapFrame.Create(bitmap2);
-            //encoder.Frames.Add(frame2);
-             
+            //bitmap1.Render(drawingVisual);
+            //bitmap2.Render(imgControl);
+            bitmap2.Render(drawingVisual2);
+            //BitmapFrame frame1 = BitmapFrame.Create(bitmap1);
+            //encoder.Frames.Add(frame1);
+            BitmapFrame frame2 = BitmapFrame.Create(bitmap2);
+            encoder.Frames.Add(frame2);
+
             using (var stream = File.Create(fileName))
             {
                 encoder.Save(stream);
@@ -172,6 +194,16 @@ namespace ImageRotator
         private void txtAngle_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+#if DEBUG
+            this.inputImageFileName = @"c:\tmp\input.bmp";
+            OpenImageFile(this.inputImageFileName);
+            this.txtAngle.Text = "120";
+            SaveRotateImage();
+#endif
         }
     }
 }
